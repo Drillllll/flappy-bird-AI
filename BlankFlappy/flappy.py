f199614926a63190fc71ce1f1e622318d86e1ca1
Kondziow <konradwojtanowski02@gmail.com>
2023-05-22 21:01:59 +0200

    Tablice (nw czy dobrze)

--- BlankFlappy/flappy.py
+++ BlankFlappy/flappy.py
@@ -2,6 +2,7 @@ import random
 import pygame.transform
 import neat
 import os
+import matplotlib.pyplot as plt
 
 # Inicjalizacja Pygame
 pygame.init()
@@ -64,10 +65,13 @@ all_sprites = pygame.sprite.Group()
 # Tworzenie rur i dodawanie do grupy obiektów
 pipe_group = pygame.sprite.Group()
 
+#Statystyki
+my_stats = []
+
 
 # Funkcja do generowania rur
 def generate_pipes():
-    pipe_gap = 200  # Odstęp pomiędzy rurami
+    pipe_gap = 90 # Odstęp pomiędzy rurami
     pipe_y = random.randint(-200, 0)
     pipe_bottom = Pipe(WIDTH, pipe_y, flipped=True)
     pipe_top = Pipe(WIDTH, pipe_y + pipe_bottom.rect.height + pipe_gap)
@@ -98,8 +102,13 @@ def eval_genomes(genomes, config):
         g.fitness = 0
         ge.append(g)
 
-    while running:
-        #clock.tick(30)
+    if(len(pipe_group) > 0):
+        for p in pipe_group:
+            pipe_group.remove(p)
+            all_sprites.remove(p)
+
+    while running and len(birds) > 0:
+        clock.tick(10000)
 
         # Generowanie nowych rur
         if len(pipe_group) < 2:
@@ -133,7 +142,7 @@ def eval_genomes(genomes, config):
                 if output[0] > 0.5:
                     bird.flap()
             else:
-                if abs(bird.rect.y - HEIGHT) < 100:
+                if abs(bird.rect.y - HEIGHT) < 200:
                     bird.flap()
 
 
@@ -153,7 +162,7 @@ def eval_genomes(genomes, config):
                 if pipe.rect.right < bird.rect.left and not pipe.passed and pipe.rect.left < bird.rect.left:
                     pipe.passed = True
                     score += 1
-                    ge[x].fitness += 5
+                    ge[x].fitness += 10
 
             # Sprawdzanie, czy ptak wyszedł poza ekran
             if bird.rect.y > HEIGHT or bird.rect.y < 0:
@@ -176,17 +185,23 @@ def eval_genomes(genomes, config):
         screen.blit(background_image, (0, 0))  # Narysowanie tła
         all_sprites.draw(screen)
 
+        #print("Ile jest w tablicy: ", len(birds))
+        #i = 0
         for bird in birds:
             screen.blit(bird.image, bird.rect)
+            #i += 1
+            #print("PTAK ",i, " X: ", bird.rect.x, " Y: ", bird.rect.y)
+        #print("Ile sie wyswietlaa: ", i)
 
 
         # Wyświetlanie wyniku
-        score_text = font.render("Score: " + str(score), True, (0, 0, 0))
+        score_text = font.render("Score: " + str(score/2), True, (0, 0, 0))
         screen.blit(score_text, (10, 10))
 
         # Aktualizacja ekranu
         pygame.display.flip()
-        clock.tick(60)
+        #clock.tick(60)
+    my_stats.append(score/2)
 
 
 
@@ -211,10 +226,23 @@ def run(config_file):
     # p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 50 generations.
-    winner = p.run(eval_genomes, 50)
+    winner = p.run(eval_genomes, 20)
 
     # show final stats
-    print('\nBest genome:\n{!s}'.format(winner))
+    #print('\nBest genome:\n{!s}'.format(winner))
+
+    for i in range(len(my_stats)):
+        print(my_stats[i])
+
+    # Tworzenie wykresu liniowego
+    plt.plot(my_stats)
+
+    # Dodanie etykiet osi x i y
+    plt.xlabel('Numer pomiaru')
+    plt.ylabel('Wynik')
+
+    # Wyświetlenie wykresu
+    plt.show()
 
 
 if __name__ == '__main__':
